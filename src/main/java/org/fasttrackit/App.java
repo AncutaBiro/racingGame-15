b675a82780beff4f5d32074efafef881fc5aeb5f
AncutaBiro <ancuta.biro@yahoo.com>
2020-03-16 21:29:50 +0200

    added read only property example

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -22,6 +22,7 @@ public class App
 
 //        Vehicle.totalVehicleCount = 50;
 
+
         System.out.println("Total count from vechicle1: " + vehicle1.getTotalVehicleCount());
         System.out.println("Total count from vechicle1: " + vehicle2.getTotalVehicleCount());
         System.out.println("Total count from Vehicle Class: " + Vehicle.getTotalVehicleCount());
