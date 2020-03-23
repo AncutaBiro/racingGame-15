46f8775d467f3eea0522b05da95b4e2cf8342911
AncutaBiro <ancuta.biro@yahoo.com>
2020-03-23 21:07:06 +0200

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,117 +11,117 @@ public class App
 {
     public static void main( String[] args )
     {
-//        Game game =  new Game ();
-//        game.start ();
+        Game game =  new Game ();
+        game.start ();
 
 
-        // method implementation taken from object's class not from variable's class
-        Vehicle cheater = new CheatingVehicle();
-        cheater.setName("Cheater");
-
-        cheater.accelerate(60,1);
-
-        System.out.println("Total distance: " + cheater.getTotalDistance());
-
-
-        // variable type decided what methods can be invoked
-        // type casting
-        ((CheatingVehicle)cheater).fly();
-
-
-
-
-
-
-
-
-
-
-        Vehicle vehicle1 = new Vehicle();
-//        vehicle1.totalVehicleCount = 1;
-
-        Vehicle vehicle2 = new Vehicle();
-//        vehicle2.totalVehicleCount = 2;
-
-//        Vehicle.totalVehicleCount = 50;
-
-        System.out.println("Total count from vechicle1: " + vehicle1.getTotalVehicleCount());
-        System.out.println("Total count from vechicle1: " + vehicle2.getTotalVehicleCount());
-        System.out.println("Total count from Vehicle Class: " + Vehicle.getTotalVehicleCount());
-
-
-//        System.out.println( "Please enter vehicle name: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String vehicleName = scanner.nextLine();
+//        // method implementation taken from object's class not from variable's class
+//        Vehicle cheater = new CheatingVehicle();
+//        cheater.setName("Cheater");
 //
-//        System.out.println("Entered name: " + vehicleName);
+//        cheater.accelerate(60,1);
 //
-//        System.out.println("Welcome to racing game!");
+//        System.out.println("Total distance: " + cheater.getTotalDistance());
 //
-        Engine engine1 = new Engine();
-        engine1.manufacturer =  "VW";
-        engine1.capacity = 2000;
-
-        //instance of the class Car
-        Car carReference = new Car (engine1);
-        carReference.setName ("Audi");
-        carReference.setColor("red");
-        carReference.setMaxSpeed(260);
-        carReference.setMileage(6.2);
-        carReference.setFuelLevel(100);
-        carReference.setDamaged(true);
-
-        System.out.println(carReference);
-
-        System.out.println("Name: " + carReference.getName());
-
-//        carReference.engine = new Engine();
-//        carReference.engine.manufacturer = "VW";
-//        carReference.engine.capacity = 2000;
-
 //
-        //apelarea metodei
-        double currentDistance = carReference.accelerate(60, 1);
-        System.out.println( "Current distance: " + currentDistance);
-
-        Mechanic mechanic = new Mechanic ();
-        mechanic.repair(carReference);
-
-//        System.out.println( "Is car damaged? " + carReference.damaged);
-
+//        // variable type decided what methods can be invoked
+//        // type casting
+//        ((CheatingVehicle)cheater).fly();
 //
-//        // one object with 2 references
-//        System.out.println( "Engine details:");
-//        System.out.println(engine1.capacity);
-//        System.out.println(carReference.engine.capacity);
 //
-//        engine1.capacity = 2100;
 //
-//        System.out.println( "Updated engine details:");
-//        System.out.println(engine1.capacity);
-//        System.out.println(carReference.engine.capacity);
 //
-//        // concatenation= lipirea mai multor stringuri unul langa altul
-//        System.out.println("Properties of " + carReference.name);
-//        System.out.println("color: " + carReference.color);
-//        System.out.println("max speed: " + carReference.maxSpeed);
-//        System.out.println("mileage: " + carReference.mileage);
-//        System.out.println("fuel level: " + carReference.fuelLevel);
-//        System.out.println("racing number: " + carReference.racingNumber);
-//        System.out.println("damaged: " + carReference.damaged);
 //
-//        Car car2 = new Car(new Engine());
-//        car2.name = "Lamborghini";
-//        car2.damaged = true;
 //
-//        System.out.println("Properties of " + car2.name);
-//        System.out.println("color: " + car2.color);
-//        System.out.println("damaged: " + car2.damaged);
-
-        // modulo (modulus operator), rezulta restul impartirii
-//       System.out.println(4 % 2 );
-//        System.out.println(4 % 2 == 0);
-//       System.out.println(4 % 3);
+//
+//
+//
+//
+//        Vehicle vehicle1 = new Vehicle();
+////        vehicle1.totalVehicleCount = 1;
+//
+//        Vehicle vehicle2 = new Vehicle();
+////        vehicle2.totalVehicleCount = 2;
+//
+////        Vehicle.totalVehicleCount = 50;
+//
+//        System.out.println("Total count from vechicle1: " + vehicle1.getTotalVehicleCount());
+//        System.out.println("Total count from vechicle1: " + vehicle2.getTotalVehicleCount());
+//        System.out.println("Total count from Vehicle Class: " + Vehicle.getTotalVehicleCount());
+//
+//
+////        System.out.println( "Please enter vehicle name: ");
+////        Scanner scanner = new Scanner(System.in);
+////        String vehicleName = scanner.nextLine();
+////
+////        System.out.println("Entered name: " + vehicleName);
+////
+////        System.out.println("Welcome to racing game!");
+////
+//        Engine engine1 = new Engine();
+//        engine1.manufacturer =  "VW";
+//        engine1.capacity = 2000;
+//
+//        //instance of the class Car
+//        Car carReference = new Car (engine1);
+//        carReference.setName ("Audi");
+//        carReference.setColor("red");
+//        carReference.setMaxSpeed(260);
+//        carReference.setMileage(6.2);
+//        carReference.setFuelLevel(100);
+//        carReference.setDamaged(true);
+//
+//        System.out.println(carReference);
+//
+//        System.out.println("Name: " + carReference.getName());
+//
+////        carReference.engine = new Engine();
+////        carReference.engine.manufacturer = "VW";
+////        carReference.engine.capacity = 2000;
+//
+////
+//        //apelarea metodei
+//        double currentDistance = carReference.accelerate(60, 1);
+//        System.out.println( "Current distance: " + currentDistance);
+//
+//        Mechanic mechanic = new Mechanic ();
+//        mechanic.repair(carReference);
+//
+////        System.out.println( "Is car damaged? " + carReference.damaged);
+//
+////
+////        // one object with 2 references
+////        System.out.println( "Engine details:");
+////        System.out.println(engine1.capacity);
+////        System.out.println(carReference.engine.capacity);
+////
+////        engine1.capacity = 2100;
+////
+////        System.out.println( "Updated engine details:");
+////        System.out.println(engine1.capacity);
+////        System.out.println(carReference.engine.capacity);
+////
+////        // concatenation= lipirea mai multor stringuri unul langa altul
+////        System.out.println("Properties of " + carReference.name);
+////        System.out.println("color: " + carReference.color);
+////        System.out.println("max speed: " + carReference.maxSpeed);
+////        System.out.println("mileage: " + carReference.mileage);
+////        System.out.println("fuel level: " + carReference.fuelLevel);
+////        System.out.println("racing number: " + carReference.racingNumber);
+////        System.out.println("damaged: " + carReference.damaged);
+////
+////        Car car2 = new Car(new Engine());
+////        car2.name = "Lamborghini";
+////        car2.damaged = true;
+////
+////        System.out.println("Properties of " + car2.name);
+////        System.out.println("color: " + car2.color);
+////        System.out.println("damaged: " + car2.damaged);
+//
+//        // modulo (modulus operator), rezulta restul impartirii
+////       System.out.println(4 % 2 );
+////        System.out.println(4 % 2 == 0);
+////       System.out.println(4 % 3);
 
 
     }
